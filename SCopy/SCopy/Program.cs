ead9eeb7630452b57a675e8dc72c829653a1aa37
javametro <minda1949@sina.com>
2019-10-17 10:07:30 +0800

    add Scopy

---
+++ SCopy/SCopy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Need two arguments:");
+                Console.WriteLine("SCopy.exe source_folder target_folder");
+                return;
+            }
+
+            string source_dir = args[0];
+            string target_dir = args[1];
+            Console.WriteLine(source_dir + " to " + target_dir);
+
+            if (!Directory.Exists(source_dir))
+            {
+                Console.WriteLine("Source directory is not exist");
+                return;
+            }
+
+            if (!Directory.Exists(target_dir))
+            {
+                Directory.CreateDirectory(target_dir);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SCopy/SCopy/Util/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCopy.Util
+{
+    public class FileHelper
+    {
+        public static bool FileExist(string file)
+        {
+            return false;
+        }
+    }
+}
